9b04e54487c566bb6d9fed8013b045ce024789bf
Jeremmy González <148731376+HECHEMAMA@users.noreply.github.com>
2024-07-22 06:13:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const alph = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz,.;:?¿¡!'Çç@$%&/º-_*0123456789";
+
+function password_generator(length) {
+    let password = ""
+    for (let i = 0; i <= length; i++) {
+        password += alph.charAt(Math.floor(Math.random() * alph.length))
+    }
+    return password;
+}
+
+// console.log(password_generator())
+
+const range__length = document.getElementById("length")
+const value__range = document.getElementById("value")
+const generate = document.getElementById("generate")
+const password__generate = document.querySelector("#password")
+
+
+value__range.innerHTML = range__length.value;
+
+range__length.oninput = function () {
+    value__range.innerHTML = this.value
+}
+
+range__length.addEventListener("mousemove", function () {
+    var x = range__length.value;
+    var color = 'linear-gradient(90deg, rgba(135, 255, 135, 0.904)' + ((.05 * x) * 100) + '%, rgb(255, 124, 124)' + ((.05 * x) * 100) + '%)';
+    range__length.style.background = color;
+})
+
+generate.addEventListener("click", function () {
+    let clave = password_generator(range__length.value);
+    // console.log(clave)
+    password__generate.value = clave;
+})
